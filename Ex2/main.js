3e0a27ddb762eb114536bf17b674022f4be42bba
DangNguyenNgocAnh <91238692+DangNguyenNgocAnh@users.noreply.github.com>
2023-03-22 14:35:57 +0700

    fix html

--- Ex2/main.js
+++ Ex2/main.js
@@ -1,6 +1,6 @@
 const item2 = document.querySelector(".nav__item2");
 const item3 = document.querySelector(".nav__item3");
-const sub2 = document.querySelector(".nav__sub-item2");
+const sub3sub2 = document.querySelector(".nav__sub-item2");
 const sub3 = document.querySelector(".nav__sub-item3");
 const aTag = document.querySelectorAll("a");
 
@@ -10,19 +10,28 @@ aTag.forEach((item) => {
   };
 });
 
+
 item2.onclick = () => {
-  if (sub2.style.opacity === "0") {
-    sub2.style.opacity = "1";
-  } else if (sub2.style.display === "none") {
-    sub2.style.display = "block";
-  }
+  if(sub2.style.height === "0px") {
+    sub2.style.opacity = "1"
+    sub2.style.height = "71px"
+    sub2.style.marginBottom = "6px"
+} else {
+  sub2.style.opacity = "0"
+  sub2.style.height = "0px"
+  sub2.style.marginBottom = "0"
+}
 };
 
 
 item3.onclick = () => {
-  if (sub3.style.opacity === "0") {
-    sub3.style.opacity = "1";
-  } else if (sub3.style.display === "none") {
-    sub3.style.display = "block";
-  }
+  if(sub3.style.height === "0px") {
+    sub3.style.opacity = "1"
+    sub3.style.height = "71px"
+    sub3.style.marginBottom = "6px"
+} else {
+  sub3.style.opacity = "0"
+  sub3.style.height = "0px"
+  sub3.style.marginBottom = "0"
+}
 };
